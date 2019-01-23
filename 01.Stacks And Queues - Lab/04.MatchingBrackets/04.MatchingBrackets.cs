bd06e6d10a686e7c1e978ca403092e726967fe54
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-01-23 23:11:20 +0100

    Stacks And Queues - Lab (Upload all problems)

---
+++ 01.Stacks And Queues - Lab/04.MatchingBrackets/04.MatchingBrackets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Stack<int> symbols = new Stack<int>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '(')
+            {
+                symbols.Push(i);
+            }
+            else if (input[i] == ')')
+            {
+                var openBracketIndex = symbols.Pop();
+
+                Console.WriteLine(input.Substring(openBracketIndex, i - openBracketIndex + 1));
+            }
+        }
+    }
+}
+
