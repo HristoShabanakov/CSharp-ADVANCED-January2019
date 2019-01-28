57f29304ea6a832d9b2a206d97ba0a77da573a45
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-01-28 14:14:31 +0100

    Sets and Dictionaries Advance - Exercise

---
+++ 06.Sets and Dictionaries Advance - Exercise/06.Wardrobe/06.Wardrobe.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        var colorClothCount = new Dictionary<string, Dictionary<string, int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(" -> ");
+
+            string color = input[0];
+            string[] clothes = input[1].Split(",");
+
+            if (!colorClothCount.ContainsKey(color))
+            {
+                colorClothCount[color] = new Dictionary<string, int>();
+            }
+
+            for (int index = 0; index < clothes.Length; index++)
+            {
+                string cloth = clothes[index];
+
+                if (!colorClothCount[color].ContainsKey(cloth))
+                {
+                    colorClothCount[color][cloth] = 0;
+                }
+
+                colorClothCount[color][cloth]++;
+            }
+        }
+
+        string[] search = Console.ReadLine().Split();
+
+        string colorToSearch = search[0];
+        string clothToSearch = search[1];
+
+        foreach (var kvp in colorClothCount)
+        {
+            string color = kvp.Key;
+            var clothCount = kvp.Value;
+
+            Console.WriteLine($"{color} clothes:");
+
+            foreach (var kvpClothCount in clothCount)
+            {
+                string cloth = kvpClothCount.Key;
+                int count = kvpClothCount.Value;
+
+                if (color == colorToSearch && cloth == clothToSearch)
+                {
+                    Console.WriteLine($"* {cloth} - {count} (found!)");
+                }
+                else
+                {
+                    Console.WriteLine($"* {cloth} - {count}");
+                }
+            }
+        }
+    }
+}
+
---
+++ 06.Sets and Dictionaries Advance - Exercise/07.TheV-Logger/07.TheV-Logger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> userNames = new HashSet<string>();
+        //User followed by...
+        var userFollowers = new Dictionary<string, HashSet<string>>();
+        //User is following...
+        var userFollwing = new Dictionary<string, HashSet<string>>();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "Statistics")
+            {
+                break;
+            }
+
+            string[] splitetInput = input.Split();
+
+            if (splitetInput.Length == 4)
+            {
+                string username = splitetInput[0];
+                if(!userNames.Contains(username))
+                {
+                    userNames.Add(username);
+                    userFollowers.Add(username, new HashSet<string>()); //same function two different syntax
+                    userFollwing[username] = new HashSet<string>();
+                }
+            }
+
+            else
+            {
+                string heFollows = splitetInput[0];
+                string followed = splitetInput[2];
+
+                //A vlogger can follow as many other vloggers as he wants, 
+                //but he cannot follow himself or follow someone he is already a follower of.
+                if (userNames.Contains(heFollows) &&
+                    userNames.Contains(followed) &&
+                    heFollows != followed)
+                {
+                    userFollowers[followed].Add(heFollows);
+                    userFollwing[heFollows].Add(followed);
+                }
+            }
+        }
+
+        Console.WriteLine($"The V-Logger has a total of {userNames.Count} vloggers in its logs.");
+
+        var topUser = userFollowers
+            .OrderByDescending(x => x.Value.Count)
+            .ThenBy(x => userFollwing[x.Key].Count())
+            .FirstOrDefault();
+
+        Console.WriteLine($"1. {topUser.Key} : {topUser.Value.Count} followers, {userFollwing[topUser.Key].Count} following");
+
+        foreach (var username in topUser.Value.OrderBy(x=>x))
+        {
+            Console.WriteLine($"*  {username}");
+        }
+
+        int count = 2;
+
+        foreach (var kvp in userFollowers.Where(x=>x.Key != topUser.Key).OrderByDescending(x => x.Value.Count)
+            .ThenBy(x => userFollwing[x.Key].Count()))
+        {
+            Console.WriteLine($"{count}. {kvp.Key} : {kvp.Value.Count} followers, {userFollwing[kvp.Key].Count} following");
+            count++;
+        }
+    }
+}
+
