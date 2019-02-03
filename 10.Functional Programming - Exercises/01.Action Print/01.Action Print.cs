305d723ed1e55d092e4b598abb89b416ec6380ca
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-02-03 22:20:23 +0100

    Functional Programming - Exercises

---
+++ 10.Functional Programming - Exercises/01.Action Print/01.Action Print.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> names = Console.ReadLine().Split().ToList();
+
+        Func<string, string> format = x => $"{x}";
+
+        Action<List<string>> print = x => Console.WriteLine(string.Join(Environment.NewLine, x.Select(format)));
+
+        print(names);
+    }
+}
+
