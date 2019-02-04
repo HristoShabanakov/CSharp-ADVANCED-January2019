14964284bda6cc059957ef5a63833e40c9cf0290
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-02-04 18:31:19 +0100

    Functional Programming - Exercises

---
+++ 10.Functional Programming - Exercises/07.PredicateForNames/07.PredicateForNames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int length = int.Parse(Console.ReadLine());
+
+        Func<string, bool> findLength = x => x.Length <= length;
+        //List<string> names = Console.ReadLine().Split().Where(x => x.Length <= n).ToList();
+
+        Action<string> printNames = x => Console.WriteLine(x);
+        //Console.WriteLine(string.Join("\n", names.Where(x => x.Length <= n)));
+
+        Console.ReadLine()
+            .Split()
+            .Where(findLength)
+            .ToList()
+            .ForEach(printNames);
+    }
+}
