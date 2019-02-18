9e68f6bad9747012f37f25aaa44a418269e10ece
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-02-18 22:22:09 +0100

    Exam Preparation

---
+++ Demo Advanced Exam - 17 Feb 2019/01.Socks/01.Socks.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] leftSocks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] rightSocks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int pairOfSocks = 0;
+
+        Stack<int> leftGivenSocks = new Stack<int>(leftSocks);
+        Queue<int> rightGivenSocks = new Queue<int>(rightSocks);
+        Queue<int> createdPair = new Queue<int>();
+
+        while (leftGivenSocks.Any() && rightGivenSocks.Any())
+        {
+            
+            int firstSock = leftGivenSocks.Peek();
+            int secondSock = rightGivenSocks.Peek();
+
+            if (firstSock == secondSock)
+            {
+                rightGivenSocks.Dequeue();
+                firstSock++;
+                leftGivenSocks.Pop();
+                leftGivenSocks.Push(firstSock);
+                leftGivenSocks.Reverse();
+            }
+            else if(secondSock > firstSock)
+            {
+                leftGivenSocks.Pop();
+            }
+            else if(firstSock > secondSock)
+            {
+                pairOfSocks = firstSock + secondSock;
+                createdPair.Enqueue(pairOfSocks);
+                leftGivenSocks.Pop();
+                rightGivenSocks.Dequeue();
+            }
+        }
+        Console.WriteLine($"{createdPair.Max()}");
+        Console.WriteLine(string.Join(" ", createdPair));
+    }
+}
+
