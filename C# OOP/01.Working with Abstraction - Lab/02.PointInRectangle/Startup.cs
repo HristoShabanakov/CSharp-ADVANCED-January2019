72fd12e890eab7197088b501fe2fef51bacae5de
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-03-02 23:55:28 +0100

    C# OOP - Working with Abstraction Lab

---
+++ C# OOP/01.Working with Abstraction - Lab/02.PointInRectangle/Startup.cs
@@ -0,0 +1,35 @@
+﻿
+namespace _02.PointInRectangle
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+
+            int[] rectagnleCoordinates = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var topLeft = new Point(rectagnleCoordinates[0], rectagnleCoordinates[1]);
+            var bottomRight = new Point(rectagnleCoordinates[2], rectagnleCoordinates[3]);
+            var rectangle = new Rectangle(topLeft, bottomRight);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] pointCoordinates = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var point = new Point(pointCoordinates[0], pointCoordinates[1]);
+                Console.WriteLine(rectangle.Contains(point));
+            }
+        }
+    }
+}
