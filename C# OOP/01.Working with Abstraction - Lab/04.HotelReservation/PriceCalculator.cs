e5f866fe10b993fa6325bf1fa82edf8ca24e23f1
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-03-03 22:58:01 +0100

    Working with Abstraction - Lab

---
+++ C# OOP/01.Working with Abstraction - Lab/04.HotelReservation/PriceCalculator.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace _04.HotelReservation
+{
+    public class PriceCalculator
+    {
+        private decimal pricePerDay;
+        private int numberOfDays;
+        private Season season;
+        private Discount discount;
+
+        public PriceCalculator(decimal pricePerDay, int numberOfDays, Season season, Discount discount)
+        {
+            this.pricePerDay = pricePerDay;
+            this.numberOfDays = numberOfDays;
+            this.season = season;
+            this.discount = discount;
+        }
+        public decimal CalculatePrice()
+        {
+            int multiplier = (int)season;
+            decimal discountMultiplier = (decimal)discount / 100;
+
+            decimal priceBeforeDiscount = numberOfDays * pricePerDay * multiplier;
+            decimal discountedAmount = priceBeforeDiscount * discountMultiplier;
+            decimal finalPrice = priceBeforeDiscount - discountedAmount;
+
+            return finalPrice;
+        }
+    }
+    public enum Season
+    {
+        Autumn = 1,
+        Spring = 2,
+        Winter = 3,
+        Summer = 4
+    }
+
+    public enum Discount
+    {
+        None,
+        SecondVisit = 10,
+        VIP = 20
+    }
+}
---
+++ C# OOP/01.Working with Abstraction - Lab/04.HotelReservation/Startup.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _04.HotelReservation
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split().ToArray();
+
+            decimal pricePerDay = decimal.Parse(input[0]);
+            int numberOfDays = int.Parse(input[1]);
+            Season season = Enum.Parse<Season>(input[2]);
+            Discount discount = Discount.None;
+
+            if(input.Length == 4)
+            {
+                discount = Enum.Parse<Discount>(input[3]);
+            }
+
+            var priceCalculator = new PriceCalculator(pricePerDay, numberOfDays, season, discount);
+            Console.WriteLine(priceCalculator.CalculatePrice().ToString("f2"));
+        }
+
+    }
+}
