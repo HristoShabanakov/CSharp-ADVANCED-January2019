c19309bf059d7da90f3016a6455d921d3adf9018
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-03-07 21:19:57 +0100

    Encapsulation - Lab

---
+++ C# OOP/03.Encapsulation - Lab/03.Validation/Person.cs
@@ -0,0 +1,100 @@
+﻿namespace PersonsInfo
+{
+    using System;
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        //The validations should always be set in the properties.
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                ValidateName(value);
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                ValidateName(value);
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age must be more than 0");
+                }
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if(value < 460)
+                {
+                    throw new ArgumentException("Salary must be more than 460 BGN.");
+                }
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            this.Salary += this.Salary * percentage / 100;
+        }
+
+        private void ValidateName(string name)
+        {
+            if (name.Length < 3)
+            {
+                throw new ArgumentException($"Name must have more than 3 characters");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
