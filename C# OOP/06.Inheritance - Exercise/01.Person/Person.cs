a64a6e7e0c68fc9407ff00647cf664992bb31af0
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-03-13 23:00:10 +0100

    Inheritance - Exercise

---
+++ C# OOP/06.Inheritance - Exercise/01.Person/Person.cs
@@ -0,0 +1,55 @@
+﻿namespace _01.Person
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public virtual int Age
+        {
+            get => age;
+
+             set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age must be positive!");
+                }
+                this.age = value;
+            }
+        }
+
+        public virtual string Name
+        {
+
+            get => name;
+
+             set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Name's length should not be less than 3 symbols!");
+                }
+                this.name = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(String.Format($"Name: {this.Name}, Age: {this.Age}"));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
