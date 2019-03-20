5ed484a2aefb462d9147b22c1971dde94f249996
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-03-20 19:29:41 +0100

    Interfaces and Abstraction - Exercise

---
+++ C# OOP/08.Interfaces and Abstraction - Exercise/01.DefineAnInterfaceIperson/Citizen.cs
@@ -0,0 +1,41 @@
+﻿namespace PersonInfo
+{
+    using System;
+
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                if (value == string.Empty)
+                {
+                    throw new ArgumentException("Name cannot be empty!");
+                }
+                this.name = value;
+            }
+        }
+        public int Age
+        {
+            get => this.age;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or negative!");
+                }
+                this.age = value;
+            }
+        }
+    }
+}
