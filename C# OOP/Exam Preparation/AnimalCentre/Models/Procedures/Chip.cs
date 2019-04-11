2ad89eedc1ad8f5a1110bca573ad0edbf4e0ea59
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2019-04-11 16:52:10 +0200

    Exam Preparation I - Structure

---
+++ C# OOP/Exam Preparation/AnimalCentre/Models/Procedures/Chip.cs
@@ -0,0 +1,22 @@
+﻿namespace AnimalCentre.Models.Procedures
+{
+    using System;
+    using AnimalCentre.Models.Contracts;
+
+    public class Chip : Procedure
+    {
+        public override void DoService(IAnimal animal, int procedureTime)
+        {
+            if (animal.IsChipped)
+            {
+                throw new ArgumentException($"{animal.Name} is already chipped");
+            }
+
+            //Calling the default behavior from Procedure class.
+            base.DoService(animal, procedureTime);
+
+            animal.Happiness -= 5;
+            animal.IsChipped = true;
+        }
+    }
+}
